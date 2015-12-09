56b5d83c726a97299505c70ddb525b21bfae45ee
Eric Weinberg <weinbe@gmail.com>
2015-12-08 23:40:19 -0500

    Day 8 part 2 solution

--- java/adventOfCode/src/main/java/com/muddworks/adventOfCode/day8/Day8.java
+++ java/adventOfCode/src/main/java/com/muddworks/adventOfCode/day8/Day8.java
@@ -36,6 +36,25 @@ import static com.muddworks.adventOfCode.Utils.getInputString;
 
  For example, given the four strings above, the total number of characters of string code (2 + 5 + 10 + 6 = 23)
  minus the total number of characters in memory for string values (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
+
+ --- Part Two ---
+
+ Now, let's go the other way. In addition to finding the number of characters of code, you should now encode
+ each code representation as a new string and find the number of characters of the new encoded representation,
+ including the surrounding double quotes.
+
+ For example:
+
+ "" encodes to "\"\"", an increase from 2 characters to 6.
+ "abc" encodes to "\"abc\"", an increase from 5 characters to 9.
+ "aaa\"aaa" encodes to "\"aaa\\\"aaa\"", an increase from 10 characters to 16.
+ "\x27" encodes to "\"\\x27\"", an increase from 6 characters to 11.
+
+ Your task is to find the total number of characters to represent the newly encoded strings minus the number of
+ characters of code in each original string literal. For example, for the strings above, the total encoded length
+ (6 + 9 + 16 + 11 = 42) minus the characters in the original code representation (23, just like in the first part
+ of this puzzle) is 42 - 23 = 19.
+
  */
 public class Day8 {
 
@@ -46,13 +65,37 @@ public class Day8 {
         String inputString = getInputString(fileName);
         Day8 day8 = new Day8();
 
-        Day8Result result = day8.countCharacters(Arrays.asList(inputString.split("\r\n")));
-        logger.info("The number of codeCharacters is {} and the number of characters in memory is {}, the difference is {}",
+        List<String> strings = Arrays.asList(inputString.split("\r\n"));
+        Day8Result result = day8.stripEscapedCharacters(strings);
+        logger.info("After stripping escaped characters, the number of codeCharacters is {} and the number of characters in memory is {}, the difference is {}",
                 result.getNumberOfCodeCharacters(), result.getNumberOfMemoryCharacters(),
                 result.getNumberOfCodeCharacters()-result.getNumberOfMemoryCharacters());
+
+        //part 2 solution
+        result = day8.escapeCharacters(strings);
+        logger.info("After escaping characters, the number of codeCharacters is {} and the number of characters in memory is {}, the difference is {}",
+                result.getNumberOfCodeCharacters(), result.getNumberOfMemoryCharacters(),
+                result.getNumberOfMemoryCharacters()-result.getNumberOfCodeCharacters());
     }
 
-    public Day8Result countCharacters(List<String> strings) {
+
+    public Day8Result escapeCharacters(List<String> strings) {
+
+        int numberOfMemoryCharacters = 0;
+        int numberOfCodeCharacters = 0;
+
+        for(String string : strings) {
+            numberOfCodeCharacters+=string.length();
+            string = string.replaceAll("\\\\", "\\\\\\\\");
+            string = string.replaceAll("\"", "\\\\\"");
+            string="\""+string+"\"";
+            numberOfMemoryCharacters+=string.length();
+        }
+
+        return new Day8Result(numberOfCodeCharacters,numberOfMemoryCharacters);
+    }
+
+    public Day8Result stripEscapedCharacters(List<String> strings) {
 
         int numberOfMemoryCharacters = 0;
         int numberOfCodeCharacters = 0;
@@ -63,7 +106,6 @@ public class Day8 {
             string = string.replaceAll("\\\\x\\w{2}", "'");
             string = string.replaceAll("\\\\\"", "'");
             string = string.replaceAll("\"", "");
-            System.out.println(string);
             numberOfMemoryCharacters+=string.length();
         }
 
--- java/adventOfCode/src/test/java/com/muddworks/adventOfCode/day8/Day8Test.java
+++ java/adventOfCode/src/test/java/com/muddworks/adventOfCode/day8/Day8Test.java
@@ -17,18 +17,17 @@ public class Day8Test {
     @Parameterized.Parameters
     public static Collection<Object[]> generateData() {
         return Arrays.asList(new Object[][]{
-                {Arrays.asList("\"\""), new Day8Result(2, 0)},
-                {Arrays.asList("\"abc\""), new Day8Result(5, 3)},
-                {Arrays.asList("\"aaa\\\"aaa\""), new Day8Result(10,7)},
-                {Arrays.asList("\"\\x27\""), new Day8Result(6, 1)},
-                {Arrays.asList("\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\"" ), new Day8Result(23, 11)},
-                {Arrays.asList("\"\\xa8br\\x8bjr\\\"\"" ), new Day8Result(16, 7)},
-                {Arrays.asList("\"daz\\\\zyyxddpwk\"" ), new Day8Result(16, 13)},
-                {Arrays.asList("\"u\\\"ptk\"" ), new Day8Result(8, 5)},
-                {Arrays.asList("\"nbydghkfvmq\\\\\\xe0\\\"lfsrsvlsj\\\"i\\x61liif\"" ), new Day8Result(41, 30)},
-                {Arrays.asList("\"qludrkkvljljd\\\\xvdeum\\x4e\"" ), new Day8Result(27, 21)},
+                {Arrays.asList("\"\""), new Day8Result(2, 6)},
+                {Arrays.asList("\"abc\""), new Day8Result(5, 9)},
+                {Arrays.asList("\"aaa\\\"aaa\""), new Day8Result(10,16)},
+                {Arrays.asList("\"\\x27\""), new Day8Result(6, 11)},
+                {Arrays.asList("\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\"" ), new Day8Result(23, 42)},
 
-
+                {Arrays.asList("\"\\xa8br\\x8bjr\\\"\"" ), new Day8Result(16, 24)},
+                {Arrays.asList("\"daz\\\\zyyxddpwk\"" ), new Day8Result(16, 22)},
+                {Arrays.asList("\"u\\\"ptk\"" ), new Day8Result(8, 14)},
+                {Arrays.asList("\"nbydghkfvmq\\\\\\xe0\\\"lfsrsvlsj\\\"i\\x61liif\"" ), new Day8Result(41, 53)},
+                {Arrays.asList("\"qludrkkvljljd\\\\xvdeum\\x4e\"" ), new Day8Result(27, 34)},
 
                 });
     }
@@ -45,7 +44,7 @@ public class Day8Test {
     @Test
     public void testSolve() {
         Day8 day8 = new Day8();
-        Day8Result actualResult = day8.countCharacters(this.input);
+        Day8Result actualResult = day8.escapeCharacters(this.input);
         assertThat(actualResult, is(this.expectedResult));
     }
 }
